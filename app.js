ad12ddce3bf9395266cac4e6ad5b11343ebb50ce
Moyo <moyo@MacBook-Pro.local>
2022-05-13 20:54:27 +0300

    Module 2. Create and connecting vue app

--- app.js
+++ app.js
@@ -1,29 +1,9 @@
-// const buttonEl = document.querySelector('button');
-// const inputEl = document.querySelector('input');
-// const listEl = document.querySelector('ul');
-
-
-// function addGoal() {
-//     const enteredValue = inputEl.value;
-//     const listItemEl = document.createElement('li');
-//     listItemEl.textContent = enteredValue;
-//     listEl.appendChild(listItemEl);
-//     inputEl.value = '';
-// }
-// buttonEl.addEventListener('click', addGoal);
-
-
-Vue.createApp({
+const app = Vue.createApp({
     data() {
-        return{
-            goals: [],
-            enteredValue: ''
-        };
-    },
-    methods: {
-        addGoal() {
-            this.goals.push(this.enteredValue);
-            this.enteredValue = '';
+        return {
+            courseGoal: 'Finish the course and learn Vue!'
         }
     }
-}).mount('#app')
+});
+
+app.mount('#user-goal');
